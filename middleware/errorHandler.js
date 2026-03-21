2c047b8c37293e0a958720723b2f781c1194267c
lakshay-0423 <blakshay162@gmail.com>
2026-03-21 19:52:20 +0530

    handle mongo duplicate key errors globally

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,12 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
+  if (err.code === 11000) {
+    return res.status(400).json({
+      message: "Duplicate field value"
+    });
+  }
 
   res.status(err.status || 500).json({
-    message: err.message || "Internal Server Error"
+    message: err.message || "Server Error"
   });
 };
 
